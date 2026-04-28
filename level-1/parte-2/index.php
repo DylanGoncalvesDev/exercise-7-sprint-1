dd75434b3dc9d7d046a512224d2862cb95a87b58
Dylan Goncalves <dylanphx93@gmail.com>
2026-04-28 13:44:50 +0200

    copy and adapted the exercise six part one to the exercise seven part two

--- level-1/parte-2/index.php
+++ level-1/parte-2/index.php
@@ -2,21 +2,34 @@
 session_start();
 
  function procesarFormulario(): string {
-  $mensaje = "";
-  if (!empty($_POST["usuario"]) && !empty($_POST["email"]) && !empty($_POST["contrasena"])) {
-      $usuario = $_POST["usuario"];
-      $email = $_POST["email"];
-      $contrasena = $_POST["contrasena"];
-      $_SESSION["usuario"] = $usuario; 
-
-      $mensaje = "Sus Datos / Usuario: " . $usuario . " / Email: " . $email . " / Contraseña: " . $contrasena 
-      . " Se han Registrado Correctamente";
-  } else {
-      $mensaje = "ERROR: Datos Inexistentes";
+  if (empty($_POST["usuario"]) || empty($_POST["email"]) || empty($_POST["contrasena"])) {
+      throw new Exception("Error: Datos Inexistentes");
   }
-   return $mensaje;
+
+  $usuario = $_POST["usuario"];
+  $email = $_POST["email"];
+  $contrasena = $_POST["contrasena"];
+     
+  if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      throw new Exception("Error: El Email No es Valido");
+  }
+  
+  if (strlen($contrasena) < 8 || strlen($contrasena) > 20) {
+     throw new Exception("Error: La Contraseña no es Valida, solo se permiten entre 8 y 20 caracteres");
+  }
+
+  $_SESSION["usuario"] = $usuario;
+
+  return "Sus Datos / Usuario: " . $usuario . " / Email: " . $email . " / Contraseña: " . $contrasena 
+         . " Se han Registrado Correctamente";
+
+ }
+
+ try {
+  echo procesarFormulario ();
+} catch (Exception $error) {
+  echo $error -> getMessage();
 }
 
-echo procesarFormulario ();
-
 ?> 
+ 
